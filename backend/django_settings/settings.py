f4a39149d01d352033b281fa0ca79a7ff80d6866
kareemIOI <kareemiisafar@gmail.com>
2023-04-01 18:25:31 +0300

    db missing psswd

--- backend/django_settings/settings.py
+++ backend/django_settings/settings.py
@@ -78,6 +78,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": 'SchoolDB',
         'USER': 'manager',
+        'PASSWORD': 'manager',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
