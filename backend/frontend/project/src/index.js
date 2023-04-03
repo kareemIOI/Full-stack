66843c791d62677aebdb16c7b566ff453e9a2b82
kareemIOI <kareemiisafar@gmail.com>
2023-04-03 11:02:44 +0300

    routing

--- backend/frontend/project/src/index.js
+++ backend/frontend/project/src/index.js
@@ -5,14 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
-import {HashRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename=''>
+    <BrowserRouter basename=''>
       <App ASE_DIR/>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
